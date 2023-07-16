c8ef61c0f8209b85f1d9558f2d299cbf050ecb61
kbul <kbul@pm.me>
2023-07-16 10:27:38 +0200

    Learning: data binding from static array.

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'pm-root-test',
   template: `
-  <div> <h1> {{pageTitle}} </h1>
+  <div> <h1> {{'Title: ' + pageTitle}} </h1>
     <!-- <div>My first Component</div> -->
     <pm-products></pm-products>
   </div>
